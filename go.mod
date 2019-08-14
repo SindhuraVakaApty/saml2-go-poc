eb297092d831bb015266bbe0ca9609754e84c5fc
Sindhura-Vaka <35214387+Sindhura-Vaka@users.noreply.github.com>
2019-08-14 15:41:51 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module mymodule
+
+go 1.12
+
+require (
+	github.com/RobotsAndPencils/go-saml v0.0.0-20170520135329-fb13cb52a46b
+	github.com/beevik/etree v1.1.0 // indirect
+	github.com/jonboulle/clockwork v0.1.0 // indirect
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/russellhaering/gosaml2 v0.3.1
+	github.com/russellhaering/goxmldsig v0.0.0-20180430223755-7acd5e4a6ef7
+	github.com/stretchr/testify v1.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"crypto/x509"
+	"fmt"
+	"net/http"
+
+	"io/ioutil"
+
+	"encoding/base64"
+	"encoding/xml"
+
+	saml2 "github.com/russellhaering/gosaml2"
+	"github.com/russellhaering/gosaml2/types"
+	dsig "github.com/russellhaering/goxmldsig"
+)
+
+func main() {
+	res, err := http.Get("https://letznav-sso.auth0.com/samlp/metadata/uvjp5d2ARaArIMFfJQU47DGNsIVakqoi")
+	if err != nil {
+		panic(err)
+	}
+
+	rawMetadata, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	metadata := &types.EntityDescriptor{}
+	err = xml.Unmarshal(rawMetadata, metadata)
+	if err != nil {
+		panic(err)
+	}
+
+	certStore := dsig.MemoryX509CertificateStore{
+		Roots: []*x509.Certificate{},
+	}
+
+	for _, kd := range metadata.IDPSSODescriptor.KeyDescriptors {
+		for idx, xcert := range kd.KeyInfo.X509Data.X509Certificates {
+			if xcert.Data == "" {
+				panic(fmt.Errorf("metadata certificate(%d) must not be empty", idx))
+			}
+			certData, err := base64.StdEncoding.DecodeString(xcert.Data)
+			if err != nil {
+				panic(err)
+			}
+
+			idpCert, err := x509.ParseCertificate(certData)
+			if err != nil {
+				panic(err)
+			}
+
+			certStore.Roots = append(certStore.Roots, idpCert)
+		}
+	}
+
+	// We sign the AuthnRequest with a random key because Okta doesn't seem
+	// to verify these.
+	randomKeyStore := dsig.RandomKeyStoreForTest()
+
+	sp := &saml2.SAMLServiceProvider{
+		IdentityProviderSSOURL:      metadata.IDPSSODescriptor.SingleSignOnServices[0].Location,
+		IdentityProviderIssuer:      metadata.EntityID,
+		ServiceProviderIssuer:       "http://localhost:8080",
+		AssertionConsumerServiceURL: "http://localhost:8080/v1/_saml_callback",
+		SignAuthnRequests:           true,
+		AudienceURI:                 "http://localhost:8080",
+		IDPCertificateStore:         &certStore,
+		SPKeyStore:                  randomKeyStore,
+	}
+
+	http.HandleFunc("/v1/_saml_callback", func(rw http.ResponseWriter, req *http.Request) {
+		err := req.ParseForm()
+		if err != nil {
+			rw.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		assertionInfo, err := sp.RetrieveAssertionInfo(req.FormValue("SAMLResponse"))
+		if err != nil {
+			rw.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		// if assertionInfo.WarningInfo.InvalidTime {
+		// 	fmt.Println("notime")
+		// 	rw.WriteHeader(http.StatusForbidden)
+		// 	return
+		// }
+
+		// if assertionInfo.WarningInfo.NotInAudience {
+		// 	rw.WriteHeader(http.StatusForbidden)
+		// 	return
+		// }
+		fmt.Fprintf(rw, assertionInfo.Values.Get("nameID"))
+	})
+
+	println("Visit this URL To Authenticate:")
+	authURL, err := sp.BuildAuthURL("")
+	if err != nil {
+		panic(err)
+	}
+
+	println(authURL)
+
+	println("Supply:")
+	fmt.Printf("  SP ACS URL      : %s\n", sp.AssertionConsumerServiceURL)
+
+	err = http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
